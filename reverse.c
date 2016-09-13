1c1dc9b13989c558c293f69584e0f74c1eaaf573
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2016-09-13 03:59:31 -0500

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
 
+void invertido(char *s, int low,int up)
+{
+    char temp;
+    if (low>=up)
+        return;
+    else
+        temp=*(s+low);
+        *(s+low)=*(s+up);
+        *(s+up)=temp;
+        invertido(s,++low,--up);
+}
+main()
+{
+    char d[]="hello world";
+    invertido(d,0,strlen(d)-1);
+    printf("%s",d);
+}
